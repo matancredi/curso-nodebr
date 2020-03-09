85429859a6e8024ee44ea4557997da43ec8ee6ea
Mariana Tancredi <matancredi@hotmail.com>
2020-03-09 11:22:13 -0300

    Últimos arquivos do módulo 4

--- 05-cli/index.js
+++ 05-cli/index.js
@@ -7,9 +7,12 @@ async function main(params){
     .version('v1')
     .option('-n, --nome [value]', "Nome do heroi")
     .option ('-p, --poder [value]', "Poder do heroi")
+    .option ('-i, --id [value]', "Id do heroi")
+
     .option('-c, --cadastrar', "Cadastrar um heroi")
     .option('-l, --listar', "Listar um heroi")
-    .option('-r, --remover', "Remover um heroi")
+    .option('-r, --remover', "Remover um heroi pelo id")
+    .option('-r, --atualizar [value]', "Atualizar um heroi pelo id")
     .parse(process.argv)
 
     // O commander estava devolvendo muita coisa, fizemos esse "molde" com a classe
@@ -18,6 +21,7 @@ async function main(params){
 
     try{
         if(Commander.cadastrar){
+            delete heroi.id;
             console.log(heroi)
             const resultado = await Database.cadastrar(heroi)
             if (!resultado){
@@ -32,9 +36,31 @@ async function main(params){
             console.log(resultado)
             return;
         }
+        if(Commander.remover){
+            const resultado = await Database.remover(heroi.id);
+            if (!resultado){
+                console.error('Não foi póssível remover o heroi')
+            }
+            console.log('Heroi removido com sucesso');
+            return;
+        }
+        if(Commander.atualizar){
+            const idParaAtualizar = parseInt(Commander.atualizar);
+            delete heroi.id;
+            // Remover todas as chaves que estiverem undefined/null
+            const dado = JSON.stringify(heroi)
+            const heroiAtualizar = JSON.parse(dado);
+            const resultado = await Database.atualizar(idParaAtualizar, heroiAtualizar);
+            if (!resultado){
+                console.error('Não foi possível atualizar o heroi')
+                return;
+            }
+            console.log('Heroi atualizado com sucesso')
+        }
+
 
     } catch (error){
-    console.error('deu ruim', error)
+    console.error('Erro:', error)
 }
 
 
