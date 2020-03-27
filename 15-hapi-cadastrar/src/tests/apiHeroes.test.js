75f776d411b8614b2c1afb5e4d451cbbd56dc009
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-03-27 15:11:17 -0300

    Módulo 9 - Web services, usando hapi e joi para cadastrar

---
+++ 15-hapi-cadastrar/src/tests/apiHeroes.test.js
@@ -0,0 +1,62 @@
+const assert = require('assert')
+const api = require('./../api')
+let app = {}
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Chapolin Colorado',
+    poder: 'Marreta Biônica'
+}
+
+describe('Suite de testes da API Heroes', function () {
+    this.beforeAll(async () => {
+        app = await api
+    })
+
+    it('listar /herois', async () => {
+        const result = await app.inject ({
+            method: 'GET',
+            url: '/herois'
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(Array.isArray(dados))
+
+    })
+    it('listar /herois - deve retornar somente 3 registros', async () => {
+        const TAMANHO_LIMITE = 3
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.length === TAMANHO_LIMITE)
+    })
+    it('listar /herois - deve filtrar pelo nome', async () => {
+        const NOME = 'Mulher Maravilha'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=1000&nome=${NOME}`
+        })
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados[0].nome === NOME)
+    })
+    it('cadastrar POST /herois', async () => {
+
+        const result = await app.inject({
+            method: 'POST',
+            url: '/herois',
+            payload: JSON.stringify(MOCK_HEROI_CADASTRAR)
+        })
+
+        const statusCode = result.statusCode
+        const {message, _id} = JSON.parse(result.payload)
+        assert.ok(statusCode === 200)
+        assert.notStrictEqual(_id, undefined)
+        assert.deepEqual(message, "Heroi cadastrado com sucesso! ")
+    })
+})
