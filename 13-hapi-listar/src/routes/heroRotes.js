6d0f8268fe4cc23b2e387e558522d5a1aef6da64
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-03-27 13:16:53 -0300

    Módulo 9 - Web services, usando hapi para listar

---
+++ 13-hapi-listar/src/routes/heroRotes.js
@@ -0,0 +1,33 @@
+const BaseRoute = require('./base/baseRoute')
+
+class HeroRoutes extends BaseRoute{
+    constructor(db){
+        super()
+        this.db = db
+    }
+
+    list(){
+        return {
+            path: '/herois',
+            method: 'GET',
+            handler: (request, headers) => {
+                try{
+                    const {skip, limit, nome} = request.query
+
+                    let query = {}
+                    if (nome){
+                        query.nome = nome
+                    }
+
+                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                }
+                catch(error){
+                    console.log('Erro: ', error)
+                    return "Erro interno no servidor"
+                }
+            }
+        }
+    }
+}
+
+module.exports = HeroRoutes
