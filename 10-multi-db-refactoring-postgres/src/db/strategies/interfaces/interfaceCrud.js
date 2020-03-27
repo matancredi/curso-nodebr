e292c1e3e478e917689ab5b2f0e9f05a395a4a43
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-03-26 21:02:19 -0300

    Módulo 8 - Refatorando postgres

---
+++ 10-multi-db-refactoring-postgres/src/db/strategies/interfaces/interfaceCrud.js
@@ -0,0 +1,36 @@
+class NotImplementedException extends Error {
+    constructor() {
+        super("Not Implemented Exception")
+    }
+
+
+}
+
+// Interface para que se siga este "contrato"
+class ICrud {
+    create(item){
+        throw new NotImplementedException()
+    }
+
+    read(query){
+        throw new NotImplementedException()
+    }
+
+    update(id, item){
+        throw new NotImplementedException()
+    }
+
+    delete(id){
+        throw new NotImplementedException()
+    }
+
+    connect() {
+        throw new NotImplementedException()
+    }
+
+    isConnected() {
+        throw new NotImplementedException()
+    }
+}
+
+module.exports = ICrud
